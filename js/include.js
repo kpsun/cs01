92b1c46989de50e0e38717e2cc67b57e5397b8c9
kpsun <kps2confirm@gmail.com>
2026-03-12 19:48:18 +1100

    test area

--- js/include.js
+++ js/include.js
@@ -10,7 +10,7 @@ document.getElementById(id).innerHTML = data;
 
 document.addEventListener("DOMContentLoaded", function () {
 
-//loadHTML("test_area"), "test.html");
+loadHTML("test_area", "test.html");
 loadHTML("header", "header.html");
 loadHTML("hero_section", "hero.html");
 loadHTML("footer", "footer.html");
