8168cdd87fe4d1f7f8ac004dfce3bb353fdc51da
kpsun <kps2confirm@gmail.com>
2026-03-12 17:19:43 +1100

    main

--- js/include.js
+++ js/include.js
@@ -10,7 +10,7 @@ document.getElementById(id).innerHTML = data;
 
 document.addEventListener("DOMContentLoaded", function () {
 
-loadHTML("js_one"), "js_one.txt"):
+//loadHTML("js_one"), "js_one.txt"):
 loadHTML("header", "header.html");
 loadHTML("hero_section", "hero.html");
 loadHTML("footer", "footer.html");
