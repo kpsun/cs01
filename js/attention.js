8cafa55e664c537012fad4b3c1f4edfd97cab205
kpsun <kps2confirm@gmail.com>
2026-03-24 21:24:30 +1100

    all

--- js/attention.js
+++ js/attention.js
@@ -1,7 +1,7 @@
   document.addEventListener("DOMContentLoaded", function() {
     const overlay = document.getElementById("overlay-box");
     const closeBtn = document.querySelector(".close-btn");
-
+	console.log(closeBtn);
     closeBtn.addEventListener("click", function() {
       overlay.style.display = "none";
     });
